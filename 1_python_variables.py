9fef0eaeddff4d8cd55927d212b99dcf04b2647b
shalabhraj1990 <shalabhrajetc@gmail.com>
2026-01-14 22:37:42 +0530

    variabled example

---
+++ 1_python_variables.py
@@ -0,0 +1,35 @@
+name = "Raja"
+age = 25
+salary = 50000.50
+print("name is ", name)
+print("age is ", age)
+print("salary is ", salary)
+
+# assigning values
+var = 10
+print(var,type(var))
+var = "Hello"
+print(var,type(var))
+
+#interger
+salary = 50000
+print(salary,type(salary))
+#float
+price = 99.99
+print("price",price,type(price))
+
+#Complex
+a = 4 + 5j
+print ("complex",a , type(a))
+
+
+#String
+
+str = 'Quality'
+
+print("String :" , str)
+
+print("get the first index",str[0])
+print("get some portion of characters",str[2:5])
+#concatenation
+print(str+" thoughts")
